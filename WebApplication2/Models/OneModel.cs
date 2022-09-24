b63021ce027f211c1c1e59783d334735afa3d83d
Achtungs <56124653+Achtungs@users.noreply.github.com>
2022-09-24 17:03:58 +0300

    Добавьте файлы проекта.

---
+++ WebApplication2/Models/OneModel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class OneModel
+    {
+        public string parametr { get; set; }
+
+        public OneModel (string value)
+        {
+            parametr = value;
+        }
+    }
+}
